f5fb07acb66db2ac5e583e4a46d59ff2356cdb7f
tomual <tomual@users.noreply.github.com>
2018-06-17 15:32:30 -0500

    CodeIgniter & User authentication

---
+++ application/views/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>title</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="<?php echo base_url('css/style.css') ?>" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="title"><a href="<?php echo base_url() ?>">title</a></div>
+        <?php if ($this->user): ?>
+            <div class="menu-authed"><a href="<?php echo base_url("user/{$this->user->user_id}") ?>"><?php echo $this->user->first_name ?></a> | <a href="<?php echo base_url('user/logout') ?>">Log Out</a></div>
+        <?php else: ?>
+            <div class="menu-guest"><a href="<?php echo base_url('user/login') ?>">Log In</a> | <a href="<?php echo base_url('user/signup') ?>">Sign Up</a></div>
+        <?php endif ?>
+        <nav>
+            <ul>
+                <li><a href="<?php echo base_url() ?>">Home</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="main">
---
+++ application/views/home.php
@@ -0,0 +1,4 @@
+<?php $this->load->view('header') ?>
+<h1>Welcome to the site</h1>
+<p>Page content</p>
+<?php $this->load->view('footer') ?>
