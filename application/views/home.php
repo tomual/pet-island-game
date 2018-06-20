cafedcbb38fdf0704f6ebcb123b927abcae9c6b6
tomual <tomual@users.noreply.github.com>
2018-06-19 19:00:55 -0500

    Island and pet

--- application/views/home.php
+++ application/views/home.php
@@ -1,4 +1,62 @@
 <?php $this->load->view('header') ?>
-<h1>Welcome to the site</h1>
-<p>Page content</p>
+<h1>Cocobox</h1>
+<p>Welcome to your island</p>
+
+<style>
+	.world {
+		background: #CCC;
+		height: 500px;
+		width: 600px;
+		position: relative;
+		border-radius: 10px;
+	}
+
+	.land {
+		background: #999;
+		height: 200px;
+		width: 540px;
+		position: absolute;
+		bottom: 0;
+		margin: 30px;
+		box-sizing: border-box;
+		border-radius: 30px;
+	}
+
+	.pet .head {
+		background: #666;
+		height: 100px;
+		width: 100px;
+		left: 5px;
+		position: absolute;
+		border-radius: 100px;
+	}
+
+	.pet .body {
+		background: #666;
+		height: 100px;
+		width: 100px;
+		top: 70px;
+		position: absolute;
+		border-top-left-radius: 50px;
+		border-top-right-radius: 50px;
+		border-bottom-left-radius: 10px;
+		border-bottom-right-radius: 10px;
+	}
+
+	.pet {
+		position: relative;
+		top: -40px;
+		left: 50px;
+	}
+</style>
+
+<div class="world">
+	<div class="land">
+		<div class="pet">
+			<div class="head"></div>
+			<div class="body"></div>
+		</div>
+	</div>
+</div>
+
 <?php $this->load->view('footer') ?>
