6a477a692e6996c4508c7700c782fb49dd75ad4b
tomual <tomual@users.noreply.github.com>
2018-06-23 19:43:20 -0500

    Idle animation, sprite improvements

--- application/views/home.php
+++ application/views/home.php
@@ -37,13 +37,16 @@
     }
 
 
-    [inkscape\:label=body] path,
-    [inkscape\:label=ears] path,
-    [inkscape\:label=head] path,
-    [inkscape\:label=arm1] path,
-    [inkscape\:label=arm2] path,
-    [inkscape\:label=leg1] path,
-    [inkscape\:label=leg2] path {
+    [inkscape\:label=top],
+    [inkscape\:label=body],
+    [inkscape\:label=ears],
+    [inkscape\:label=head],
+    [inkscape\:label=arm1],
+    [inkscape\:label=arm2],
+    [inkscape\:label=leg1],
+    [inkscape\:label=leg2] {
+        transition: all 1s;
+        position: relative;
     }
 
     .bean a {
@@ -58,6 +61,49 @@
         margin-top: 50px;
     }
 </style>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script type="text/javascript">
+    var headMovePixels = 0;
+    var headRotatePixels = 0;
+    var bodyMovePixels = 0;
+    var armMovePixels = 0;
+    var armRotatePixels = 0;
+    $(document).ready(function() {
+       breathe();
+       blink();
+    });  
+
+    function breathe() {
+        headMovePixels = headMovePixels ? 0 : 3;
+        bodyMovePixels = bodyMovePixels ? 0 : -2;
+        armMovePixels = armMovePixels ? 0 : -2;
+        armRotatePixels = armRotatePixels ? 0 : 0.5;
+        headRotatePixels = headRotatePixels ? 0 : 0.5;
+        $('[inkscape\\:label=body]').css('top', bodyMovePixels);
+        $('[inkscape\\:label=top]').css('top', headMovePixels);
+        $('[inkscape\\:label=top]').css('transform', 'rotate(' + headRotatePixels + 'deg)');
+        $('[inkscape\\:label=arm1]').css('top', armMovePixels);
+        $('[inkscape\\:label=arm1]').css('transform', 'rotate(' + armRotatePixels + 'deg)');
+        $('[inkscape\\:label=arm2]').css('top', armMovePixels);
+        $('[inkscape\\:label=arm2]').css('transform', 'rotate(' + armRotatePixels + 'deg)');
+        window.setTimeout(function() { breathe() }, 1200)
+    }
+    function blink() {
+        console.log($('[inkscape\\:label=face]').css('display'));
+        $('[inkscape\\:label=face]').css('display', 'none');
+        $('[inkscape\\:label=face-blink]').css('display', 'block');
+        window.setTimeout(function() { 
+            $('[inkscape\\:label=face]').css('display', 'inline');
+            $('[inkscape\\:label=face-blink]').css('display', 'none');
+            window.setTimeout(function() { blink() }, getRandomInt(7) * 1000)
+         }, 50);
+        
+    }
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+</script>
 
 <div class="world">
     <div class="land">
