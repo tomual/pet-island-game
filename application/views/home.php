8d64a86dd8021a92cee27e8ca5e89528c52afc0a
tomual <tomual@users.noreply.github.com>
2018-07-06 23:45:19 -0500

    Mailing list functionality

--- application/views/home.php
+++ application/views/home.php
@@ -7,5 +7,16 @@ quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
+<form method="post" action="<?php echo base_url('mailing') ?>">
+	<h5>Stay up-to-date</h5>
+	<?php alerts() ?>
+	<div class="form-group">
+		<label class="form-label sr-only">Email</label>
+		<input type="email" name="email" class="form-control">
+		<small class="form-text text-muted">We'll only use your email to send you updates</small>
+	</div>
+	<div class="form-group">
+		<input type="submit" value="Submit" class="btn btn-primary">
+	</div>
+</form>
 <?php $this->load->view('footer') ?>
