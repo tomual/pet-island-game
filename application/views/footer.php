080d104a726de108c14ec52f5ea8ca71dd7a60d4
tomual <tomual@users.noreply.github.com>
2020-02-22 16:15:19 +0800

    Move container div out of header/footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,5 +1,4 @@
-			</div>
-		</main>
+
 		<script src="<?php echo base_url('js/script.js') ?>"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 	</body>
--- application/views/header.php
+++ application/views/header.php
@@ -11,41 +11,43 @@
     <link href="<?php echo base_url('css/style.css') ?>" rel="stylesheet">
 </head>
 <body>
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Title</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
+    <div class="bg-dark">
+        <div class="container"> 
+            <nav class="navbar navbar-expand-md navbar-dark">
+                <a class="navbar-brand" href="#">Title</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-        <div class="collapse navbar-collapse" id="navbar">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="<?php echo base_url() ?>">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo base_url('page') ?>">Page</a>
-                </li>
-            </ul>
-            <div class="my-2 my-lg-0">
-                <ul class="navbar-nav mr-auto">
-                    <?php if ($this->user): ?>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url('settings/edit') ?>"><?php echo $this->user->username ?></a>
+                <div class="collapse navbar-collapse" id="navbar">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="<?php echo base_url() ?>">Home <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url('user/logout') ?>">Log Out</a>
+                            <a class="nav-link" href="<?php echo base_url('page') ?>">Page</a>
                         </li>
-                    <?php else: ?>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url('user/login') ?>">Log In</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url('user/signup') ?>">Sign Up</a>
-                        </li>
-                    <?php endif ?>
-                </ul>
+                    </ul>
+                    <div class="my-2 my-lg-0">
+                        <ul class="navbar-nav mr-auto">
+                            <?php if ($this->user): ?>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo base_url('settings/edit') ?>"><?php echo $this->user->username ?></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo base_url('user/logout') ?>">Log Out</a>
+                                </li>
+                                <?php else: ?>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="<?php echo base_url('user/login') ?>">Log In</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="<?php echo base_url('user/signup') ?>">Sign Up</a>
+                                    </li>
+                                <?php endif ?>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
             </div>
         </div>
-    </nav>
-    <main role="main" class="container">
-        <div class="row">
